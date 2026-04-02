aec0b9a3dab66dfadce9bca234f83f524dbd9862
BVB-Glitch <207627199+Arran-h-portfolio@users.noreply.github.com>
2026-04-02 09:37:45 +0100

    n/a

--- obj/Debug/net10.0/ToDoListApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ToDoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18f88da1924413c3bfd51c8d1c41c584ddefc13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7beefdfab7cf93f6003c3faf5685ba875264355")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
